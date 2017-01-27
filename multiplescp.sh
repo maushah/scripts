f7328b2ae00224ad4459c77de6efc7dc0a74eddb
maushah <maushah@users.noreply.github.com>
2017-01-27 13:43:41 -0800

    Update multiplescp.sh

--- multiplescp.sh
+++ multiplescp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-for server in 10.26.31.115 10.26.31.193 10.26.33.38 10.26.32.5 10.26.32.149 10.26.33.86; do
+for server in 10.10.1.1 10.10.2.1; do
    scp -F ssh.config ubuntu@$server:~/status.xml ~/Documents/"$server"_status.xml
 done
