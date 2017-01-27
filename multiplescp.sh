aa73567a3dd2e75aca036b7a253411a405f2d204
maushah <maushah@users.noreply.github.com>
2017-01-27 13:45:01 -0800

    Update multiplescp.sh

--- multiplescp.sh
+++ multiplescp.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# SCP file from multiple servers and save with server in filename
 for server in 10.10.1.1 10.10.2.1; do
-   scp -F ssh.config ubuntu@$server:~/status.xml ~/Documents/"$server"_status.xml
+   scp ubuntu@$server:~/status.xml ~/Documents/"$server"_status.xml
 done
